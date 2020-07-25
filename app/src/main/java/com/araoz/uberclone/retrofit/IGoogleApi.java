101c0b166f0ae5b23a8cd521f544a3e3858cde7d
Sebastian <ubaldosebastian25@gmail.com>
2020-07-25 10:35:19 -0500

    commit complete?)

--- app/src/main/java/com/araoz/uberclone/retrofit/IGoogleApi.java
+++ app/src/main/java/com/araoz/uberclone/retrofit/IGoogleApi.java
@@ -1,4 +1,12 @@
 package com.araoz.uberclone.retrofit;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
 public interface IGoogleApi {
+
+    @GET
+    Call<String> getDirections(@Url String url);
+
 }
--- app/src/main/java/com/araoz/uberclone/services/MyFirebaseMessagingClient.java
+++ app/src/main/java/com/araoz/uberclone/services/MyFirebaseMessagingClient.java
@@ -1,4 +1,20 @@
 package com.araoz.uberclone.services;
 
-public class MyFirebaseMessagingClient {
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingClient extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
 }
